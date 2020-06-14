27405b509ec28a810d95dc37af19f419e5669f73
Benzir Ahammed Shawon <benzir1116@gmail.com>
2020-06-15 01:58:44 +0600

    1.0

---
+++ python_works/pattarn_codes/h_pyramid.py
@@ -0,0 +1,18 @@
+def printPattern( n) :
+	k = 0
+	for i in range(1,n+1) :
+		for j in range(i,n) :
+			print(' ', end='')
+
+		while (k != (2 * i - 1)) :
+			if (k == 0 or k == 2 * i - 2) :
+				print('*', end='')
+			else :
+				print(' ', end ='')
+			k = k + 1
+		k = 0;
+		print ("")
+	for i in range(n) :
+		print ('* ', end = '')
+n = 8
+printPattern(n)
